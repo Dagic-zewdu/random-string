69a4743c480d641f40038adfabede9b93050f581
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-27 11:43:21 +0300

    fix: reformat code using prettier

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import crypto from 'crypto';
  * @param option - could be a number which is the length of the string to be generated || could be an option {
  * @returns - random string generated
  */
-export const generate = (option: number | options): string => {  
+export const generate = (option: number | options): string => {
   let result = '';
 
   if (typeof option === 'number' || !option) {
@@ -15,17 +15,16 @@ export const generate = (option: number | options): string => {
     const characters = alphanumeric;
     result = compute(characters, opt);
   } else if (typeof option === 'object') {
-    
     const { length = 16, range: characters, charset } = option;
-    
+
     if (characters) {
       result = compute(characters, length);
       result = checkOptions(result, option);
     }
 
-    switch(charset) {
+    switch (charset) {
       case 'number':
-        const max = Math.pow(10, length) - 1; 
+        const max = Math.pow(10, length) - 1;
         const randomBytes = crypto.randomBytes(Math.ceil(length / 2));
         const randomValue = parseInt(randomBytes.toString('hex'), 16) % max; // Converting the random bytes to a number within the desired range
         result = randomValue.toString();
@@ -59,18 +58,18 @@ const compute = (type: string, length: number) => {
   const charactersLength = type.length;
   for (let i = 0; i < length; i += 1) {
     const randomBytes = crypto.randomBytes(1);
-    const randomIndex = Math.floor(randomBytes[0] / 256 * charactersLength);
+    const randomIndex = Math.floor((randomBytes[0] / 256) * charactersLength);
     computedResult += type.charAt(randomIndex);
   }
   return computedResult;
-}
+};
 
 export const generateUnicodeEmoji = (length: number = 16) => {
   let result = '';
   const charactersLength = emojis.length;
   for (let i = 0; i < length; i += 1) {
     const randomBytes = crypto.randomBytes(1);
-    const randomIndex = Math.floor(randomBytes[0] / 256 * charactersLength);
+    const randomIndex = Math.floor((randomBytes[0] / 256) * charactersLength);
     result += emojis[randomIndex].trim();
   }
   return result;
